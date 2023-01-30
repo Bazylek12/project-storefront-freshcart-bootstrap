08a7dd6822988017ea564c9a672149a45374ccb6
Bazylek12 <a.kosmowski7@gmail.com>
2023-01-30 19:39:22 +0100

    feat(): product list filtered by catId implemented

--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { CategoryModel } from '../../models/category.model';
-import { CategoriesService } from '../../services/categories.service';
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {combineLatest, Observable} from 'rxjs';
+import {map, switchMap} from 'rxjs/operators';
+import {CategoryModel} from '../../models/category.model';
+import {ProductModel} from '../../models/product.model';
+import {CategoriesService} from '../../services/categories.service';
+import {ProductsService} from '../../services/products.service';
 
 @Component({
   selector: 'app-category-products',
@@ -16,7 +18,27 @@ export class CategoryProductsComponent {
   readonly categoriesList$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
   readonly category$: Observable<CategoryModel> = this._activatedRoute.params.pipe(
     switchMap(params => this._categoriesService.getOneCategory(params['categoryId'])));
+  readonly productsList$: Observable<ProductModel[]> = combineLatest([
+    this._productsService.getAllProducts(),
+    this.category$,
+  ]).pipe(
+    map(([products, category]: [ProductModel[], CategoryModel]) =>
+      products.filter(product => product.categoryId.includes(category.id))
+    )
+  )
 
-  constructor(private _categoriesService: CategoriesService, private _activatedRoute: ActivatedRoute) {
+
+  constructor(private _categoriesService: CategoriesService, private _activatedRoute: ActivatedRoute, private _productsService: ProductsService) {
   }
+
+  public starClass(ratingValue: number, star: number): string {
+    if (ratingValue >= star) {
+      return 'bi-star-fill';
+    }
+    if (ratingValue + 1 > star && ratingValue % 1 !== 0) {
+      return 'bi-star-half';
+    }
+    return 'bi-star';
+  }
+
 }
