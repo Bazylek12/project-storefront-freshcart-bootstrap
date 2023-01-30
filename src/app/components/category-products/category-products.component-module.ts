4870ab93d21380f15476eb21ddebcadf7597d3b1
Bazylek12 <a.kosmowski7@gmail.com>
2023-01-30 20:44:48 +0100

    feat(): sort by price, featureValue and rating implemented

--- src/app/components/category-products/category-products.component-module.ts
+++ src/app/components/category-products/category-products.component-module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CategoryProductsComponent } from './category-products.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
   declarations: [CategoryProductsComponent],
   providers: [],
   exports: [CategoryProductsComponent]
--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -1,11 +1,13 @@
 import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {combineLatest, Observable} from 'rxjs';
+import {combineLatest, Observable, of, shareReplay, startWith} from 'rxjs';
 import {map, switchMap} from 'rxjs/operators';
 import {CategoryModel} from '../../models/category.model';
 import {ProductModel} from '../../models/product.model';
 import {CategoriesService} from '../../services/categories.service';
 import {ProductsService} from '../../services/products.service';
+import {FormControl} from "@angular/forms";
+import {SortingOptionModel} from "../../models/sorting-option.model";
 
 @Component({
   selector: 'app-category-products',
@@ -17,15 +19,30 @@ import {ProductsService} from '../../services/products.service';
 export class CategoryProductsComponent {
   readonly categoriesList$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
   readonly category$: Observable<CategoryModel> = this._activatedRoute.params.pipe(
-    switchMap(params => this._categoriesService.getOneCategory(params['categoryId'])));
+    switchMap(params => this._categoriesService.getOneCategory(params['categoryId'])),
+    shareReplay(1)
+  );
+  readonly sortValue: FormControl = new FormControl('featureValueDescending');
+  readonly sort$: Observable<string> = this.sortValue.valueChanges.pipe(
+    startWith('featureValueDescending'),
+    shareReplay(1)
+  );
+  readonly sortingOptions$: Observable<SortingOptionModel[]> = of([
+    { name: 'Featured', value: 'featureValueDescending' },
+    { name: 'Price: Low to High', value: 'priceAscending' },
+    { name: 'Price: High to low', value: 'priceDescending' },
+    { name: 'Avg. Rating', value: 'ratingValueDescending' }
+  ])
+
   readonly productsList$: Observable<ProductModel[]> = combineLatest([
-    this._productsService.getAllProducts(),
+    this._productsService.getAllProducts().pipe(shareReplay(1)),
     this.category$,
+    this.sort$
   ]).pipe(
-    map(([products, category]: [ProductModel[], CategoryModel]) =>
-      products.filter(product => product.categoryId.includes(category.id))
+    map(([products, category, sort]: [ProductModel[], CategoryModel, string]) =>
+      this.sortProducts(products, category, sort)
     )
-  )
+  );
 
 
   constructor(private _categoriesService: CategoriesService, private _activatedRoute: ActivatedRoute, private _productsService: ProductsService) {
@@ -39,6 +56,26 @@ export class CategoryProductsComponent {
       return 'bi-star-half';
     }
     return 'bi-star';
+  }
+
+  public sortProducts(products: ProductModel[], category: CategoryModel, sort: string): ProductModel[] {
+    return products
+      .filter((product) => product.categoryId.includes(category.id))
+      .sort((a, b) => {
+        if (sort === 'featureValueDescending') {
+         return a.featureValue < b.featureValue ? 1 : -1;
+        }
+        if (sort === 'priceAscending') {
+          return a.price > b.price ? 1 : -1;
+        }
+        if (sort === 'priceDescending') {
+          return a.price < b.price ? 1 : -1;
+        }
+        if (sort === 'ratingValueDescending') {
+          return a.ratingValue < b.ratingValue ? 1 : -1;
+        }
+        return 0;
+      });
   }
 
 }
